17f06e772b0c3eac0e639f6f709cb97e62a54d04
shunsuke <sasakishunsuke1010@gmail.com>
2026-03-23 16:52:54 +0900

    initial version

---
+++ tpm_emulator.sh
@@ -0,0 +1,6 @@
+sudo apt install swtpm
+mkdir -p /tmp/emulated_tpm
+swtpm socket --tpmstate dir=/tmp/emulated_tpm \
+         --ctrl type=unixio,path=/tmp/emulated_tpm/swtpm-sock \
+         --log level=20 --tpm2 --daemon
+
